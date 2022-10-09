e94f12c7159c6e8b57a79303bcd110715c49afc0
Joshua <buildingguy@sbcglobal.net>
2022-10-09 14:04:20 -0700

    psuedocode

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // tests for customer
         Customer customerA = new Customer("Abby", 1000);
         System.out.println("Name: " + customerA.getName());
         System.out.println("Balance: $" + customerA.getBalance());
