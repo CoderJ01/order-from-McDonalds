8b16a16ab8f02e961899a90c0cb2333d986d8963
Joshua <buildingguy@sbcglobal.net>
2022-10-09 15:49:39 -0700

    change object name

--- Main.java
+++ Main.java
@@ -9,25 +9,25 @@ public class Main {
         System.out.println(" ");
 
         // test for menu item
-        MenuItem itemA = new MenuItem("Big Mac", 3.99, 563);
-        System.out.println("Name: " + itemA.getItem());
-        System.out.println("Cost: $" + itemA.getCost());
-        System.out.println("Calories: " + itemA.getCalories()); 
+        MenuItem BigMac = new MenuItem("Big Mac", 3.99, 563);
+        System.out.println("Name: " + BigMac.getItem());
+        System.out.println("Cost: $" + BigMac.getCost());
+        System.out.println("Calories: " + BigMac.getCalories()); 
 
         // space 
         System.out.println(" ");
 
-        MenuItem itemB = new MenuItem("Medium Fries", 1.79, 378);
-        System.out.println("Name: " + itemB.getItem());
-        System.out.println("Cost: $" + itemB.getCost());
-        System.out.println("Calories: " + itemB.getCalories()); 
+        MenuItem mediumFry = new MenuItem("Medium Fries", 1.79, 378);
+        System.out.println("Name: " + mediumFry.getItem());
+        System.out.println("Cost: $" + mediumFry.getCost());
+        System.out.println("Calories: " + mediumFry.getCalories()); 
 
         // space 
         System.out.println(" ");
 
         // add item
-        customerA.addItem(itemA);
-        customerA.addItem(itemB);
+        customerA.addItem(BigMac);
+        customerA.addItem(mediumFry);
         
         customerA.getTotalCost();
         
