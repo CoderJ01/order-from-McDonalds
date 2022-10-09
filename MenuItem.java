176d0a9d7083f50a7459be763e79c466d9db679d
Joshua <buildingguy@sbcglobal.net>
2022-10-09 14:10:28 -0700

    psuedocode

--- MenuItem.java
+++ MenuItem.java
@@ -1,14 +1,17 @@
 public class MenuItem {
+    // variables
     private String item;
     private double cost;
     private int calorieAmount;
 
+    // constructor
     public MenuItem(String item, double cost, int calorieAmount) {
         this.item = item;
         this.cost = cost;
         this.calorieAmount = calorieAmount;
     }
 
+    // getters
     public String getItem() {
         return this.item;
     }
