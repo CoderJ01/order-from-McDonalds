5d8963580edcf3c2d042d8d15988827275ae8816
Joshua <buildingguy@sbcglobal.net>
2022-10-09 14:08:03 -0700

    MenuItem constructor

--- MenuItem.java
+++ MenuItem.java
@@ -1,3 +1,11 @@
 public class MenuItem {
-    
+    private String item;
+    private double cost;
+    private int calorieAmount;
+
+    public MenuItem(String item, double cost, int calorieAmount) {
+        this.item = item;
+        this.cost = cost;
+        this.calorieAmount = calorieAmount;
+    }
 }
