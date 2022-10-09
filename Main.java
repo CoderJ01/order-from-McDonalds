7b92cec01f985e32f47e448abb84a5a28eaf5c10
Joshua <buildingguy@sbcglobal.net>
2022-10-09 13:51:03 -0700

    test getters and setters

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Customer customerA = new Customer("", 0);
+        customerA.setName("Abby");
+        customerA.setBalance(1000.35);
+        System.out.println("Name: " + customerA.getName());
+        System.out.println("Balance: $" + customerA.getBalance());
     }
 }
