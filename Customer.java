3f26ff471a86aaba6a8ddc6202448a87a8a5fa0d
Joshua <buildingguy@sbcglobal.net>
2022-10-09 13:53:20 -0700

    clean (erase un-needed setters)

--- Customer.java
+++ Customer.java
@@ -17,13 +17,4 @@ public class Customer {
     public double getBalance() {
         return this.balance;
     }
-
-    // setters
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
 }
