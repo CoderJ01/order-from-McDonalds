2b3b492f74db541e79dccacd8497f44194a4d629
Joshua <buildingguy@sbcglobal.net>
2022-10-09 13:42:08 -0700

    psuedocode'

--- Customer.java
+++ Customer.java
@@ -1,7 +1,9 @@
 public class Customer {
+    // variables
     private String name;
     private double balance;
 
+    // constructor
     public Customer(String name, double balance) {
         this.name = name;
         this.balance = balance;
