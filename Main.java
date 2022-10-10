e528c15abac1e0ffe469d53e711ac824294fd9ec
Joshua <buildingguy@sbcglobal.net>
2022-10-09 18:05:37 -0700

    import Scanner

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         // burgers
         MenuItem BigMac = new MenuItem("Big Mac", 3.99, 563);
