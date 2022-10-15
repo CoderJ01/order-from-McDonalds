61a85970442a8d1237b919bf05f338cd9ebaf180
Joshua <buildingguy@sbcglobal.net>
2022-10-15 09:07:03 -0700

    clean

--- Main.java
+++ Main.java
@@ -7,22 +7,7 @@ public class Main {
     private static Scanner input = new Scanner(System.in);
     private static List<MenuItem> items = new ArrayList<>(); // hold array of menu items
     public static void main(String[] args) {
-        // burgers
-        MenuItem BigMac = new MenuItem("Big Mac", 3.99, 563);
-        MenuItem QuarterPounder = new MenuItem("Quarter Pounder", 3.79, 520);
-        MenuItem doubleCheeseburger = new MenuItem("Double Cheeseburger", 1.69, 450);
-        MenuItem cheeseburger = new MenuItem("cheeseburger", 1.00, 300);
-        MenuItem hamburger = new MenuItem("cheeseburger", 1.00, 300);
-
-        // fries
-        MenuItem smallFry = new MenuItem("small fry", 1.39, 230);
-        MenuItem mediumFry = new MenuItem("medium fry", 1.79, 320);
-        MenuItem largeFry = new MenuItem("large fry", 1.89, 480);
-
-        // desserts
-        MenuItem cookies = new MenuItem("bag of cookies", 1.00, 510);
-        MenuItem bakedApplePie = new MenuItem("apple pie", 0.99, 230);
-
+        
         // burgers
         items.add(new MenuItem("hamburger", 1.00, 300));
         items.add(new MenuItem("cheeseburger", 1.00, 300));
